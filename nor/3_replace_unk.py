365530cadbd2a8b36a89b76db6c4aaecb757a567
dan <phamvandan.cntt2@gmail.com>
2021-01-10 16:18:09 +0700

    multi

--- nor/3_replace_unk.py
+++ nor/3_replace_unk.py
@@ -1,4 +1,5 @@
 import os
+import multiprocessing
 
 import pandas as pd
 UNK = "*"
@@ -28,35 +29,61 @@ def load_unks():
 
 load_unks()
 
+save_dir = os.path.join(root, "nor_unk")
+def process(filename):
+    print("PROCESS", filename)
+    f = open(os.path.join(dir, filename))
+    f1 = open(os.path.join(save_dir, filename), "w+")
+    while True:
+        sentence = f.readline().replace("\n", "")
+        if sentence == "":
+            break
+        temp = sentence.split("\t")
+        temp = temp[0] + "\t" + temp[1] + "\t" + temp[2]
+        sentence = sentence.split("\t")[3]
+        words = sentence.split(" ")
+        for index, word in enumerate(words):
+            if word in unks:
+                words[index] = UNK
+            else:
+                words[index] = s_codes[s_words.index(word)]
+        sentence = " ".join(words)
+        temp = temp + "\t" + sentence
+        f1.write(temp + "\n")
 
 def unk_from_lst(dir, save_dir):
     filenames = os.listdir(dir)
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
+    p = multiprocessing.Pool()
     for filename in filenames:
-        if ".lst" not in filename:
-            continue
-        print("PROCESS", filename)
-        f = open(os.path.join(dir, filename))
-        f1 = open(os.path.join(save_dir, filename), "w+")
-        while True:
-            sentence = f.readline().replace("\n", "")
-            if sentence == "":
-                break
-            temp = sentence.split("\t")
-            temp = temp[0] + "\t" + temp[1] + "\t" + temp[2]
-            sentence = sentence.split("\t")[3]
-            words = sentence.split(" ")
-            for index, word in enumerate(words):
-                if word in unks:
-                    words[index] = UNK
-                else:
-                    words[index] = s_codes[s_words.index(word)]
-            sentence = " ".join(words)
-            temp = temp + "\t" + sentence
-            f1.write(temp + "\n")
-        f.close()
-        f1.close()
+        p.apply_async(process, [filename]) 
+
+    #     if ".lst" not in filename:
+    #         continue
+    #     print("PROCESS", filename)
+    #     f = open(os.path.join(dir, filename))
+    #     f1 = open(os.path.join(save_dir, filename), "w+")
+    #     while True:
+    #         sentence = f.readline().replace("\n", "")
+    #         if sentence == "":
+    #             break
+    #         temp = sentence.split("\t")
+    #         temp = temp[0] + "\t" + temp[1] + "\t" + temp[2]
+    #         sentence = sentence.split("\t")[3]
+    #         words = sentence.split(" ")
+    #         for index, word in enumerate(words):
+    #             if word in unks:
+    #                 words[index] = UNK
+    #             else:
+    #                 words[index] = s_codes[s_words.index(word)]
+    #         sentence = " ".join(words)
+    #         temp = temp + "\t" + sentence
+    #         f1.write(temp + "\n")
+    #     f.close()
+    #     f1.close()
+    p.close()
+    p.join() # Wait for all child processes to close
 
 
 unk_from_lst(os.path.join(root, "nor_number"), os.path.join(root, "nor_unk"))
